811232b31bfdd5c9bb9800e928747de44e4da807
Anamika147 <60608643+Anamika147@users.noreply.github.com>
2022-05-10 20:33:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,50 +1,78 @@
-# IBM_Group_29
+# Foodie(food delivery)
 
-Theme: Digitization to ease to access to social services
-
-
-Problem statement :
-Many needful people in a locality , often is deprived of atleast one time food. 
-And what is better than homely food.
-
-But unfortunately,  lot of food is wasted on a daily basis . 
-
-
-Solution :
-Connecting homes, individuals and restaurants, in locality , and creating a database ,  to monitor  if any good amount of  fresh food is left , on a daily basis , which can be provided to the needy . 
-Wastage of food can be minimized in larger quantities.
-
-
-Personas of the system:
-- Orphanages
-- Old age homes
-- Child care centres
-- Hospitals
-
-
-Architecture : 
-Website
-- MySQL
-- PHP
-- HTML
-- CSS
-- Javascript
-
-
-![Screenshot (93)](https://user-images.githubusercontent.com/85350031/167126792-7334ee88-1516-43d6-a095-6c327afbc585.png)
-
-Presentation PPT link : https://docs.google.com/presentation/d/1nHQ2f_RiKI8_-qSeYlba0SCDR3I-ltzG/edit?usp=sharing&ouid=106181210741482713527&rtpof=true&sd=true
-
-Presentation video link : https://drive.google.com/file/d/1yW8vIpgzk9MW1S-dZJV-EwqGMkcmfxJo/view?usp=sharing
+A simple raw PHP & Javascript based website that is similar to foodpanda. Users can order food online from restaurants from different locations. And the admin can check out all the processes like users, deliveries, total sales etc.
+<p align="center">
+   <br><br>
+  <img src="https://image.ibb.co/hzDnLq/Screenshot-2018-10-29-Foodie.png" alt="Screenshot-2018-10-29-Foodie" border="0">
+  <br><br>
+   Login Page
+   <br><br>
+  <img src="https://image.ibb.co/jphE0q/Screenshot-2018-10-29-Foodie-1.jpg" alt="Screenshot-2018-10-29-Foodie-1" border="0">
+  <br><br>
+  Sign Up
+  <br><br>
+  <img src="https://image.ibb.co/gbZSLq/Screenshot-2018-10-29-Foodie.jpg" alt="Screenshot-2018-10-29-Foodie" border="0">
+  <br><br>
+  Adding to cart
+  <br><br>
+  <img src="https://image.ibb.co/bHKu0q/Screenshot-2018-10-29-Foodie-2.png" alt="Screenshot-2018-10-29-Foodie-2" border="0">
+  <br><br>
+  Payment
+   <br><br>
+  <img src="https://image.ibb.co/k0O9YA/Screenshot-2018-10-29-Foodie-3.png" alt="Screenshot-2018-10-29-Foodie-3" border="0">
+  <br><br>
+  Admin
+   <br><br>
+  <img src="https://image.ibb.co/gwYXmV/Screenshot-2018-10-29-Admin-Panel.png" alt="Screenshot-2018-10-29-Admin-Panel" border="0">
+</p>
 
 
 
-Contributors:
-Frontend UI         :  Anamika A                          
-                       Shuaib Muhammed T A   
-                       Joyal George 
 
-Presentation slides : Bincy Daniel
+## Getting Started
 
-Video               : Bonia Benny
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+You will need Xampp installed on your pc or an online php hosting.
+
+### Installing
+
+Download Xampp from this link: https://www.apachefriends.org/download.html and install it.
+
+## Deployment
+
+Deployment on a local PC:
+*	Copy all the files to a folder called “Foodie”.
+*	Copy the folder “Foodie” to a folder inside the directory where XAMPP is installed, called “htdocs”.
+*	Open XAMPP and press the first two “Start” buttons to turn your PC into the server.
+*	Open a web browser (i.e. Google Chrome or Mozilla Firefox).
+*	Type in the following address: localhost/Foodie.
+*	Now, type in the following address: localhost/phpmyadmin
+*	Import the .sql file from the “database” folder, which is inside the “Foodie” folder of “htdocs”.
+Now, you can use our website on your PC.
+
+Deployment online: 
+*	Let’s assume we bought the domain “foodie.com”.
+*	We will also need to rent a web-server and bandwidth from a hosting service.
+*	The hosting service will give us access to the server, where we can upload files and install custom scripts to keep our site up and running.
+
+## Built With
+
+* [PHP](http://php.net/) - The backend was developed using PHP
+* [JavaScript](https://www.javascript.com/) - For front end, we also used Jquery, Bootstrap
+
+## Authors
+
+* **A. K. M. Saiful Islam** - *Initial work* - [adar2378](https://github.com/adar2378)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+## Acknowledgments
+
+* It was a collaborative project with my classmates, hatts off to them for putting things together. :blush:
+* And learned a lot from various youtube videos :relaxed:
 
---
+++ about_us.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="assets/css/half-slider.css" rel ="stylesheet">
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/half-slider.css" rel="stylesheet">
+<link href="assets/css/index.css" rel="stylesheet">
+  <script src="assets/js/source.bootstrap.min.js"></script>
+  <script src="assets/js/jquery.min.js"></script>
+    <title>Zero Waste Zero Hunger</title>
+
+  </head>
+
+  <body>
+
+<?php
+  include "nav.php";
+?>
+
+    <header>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="carousel-item item active">
+      <img style="height:400px; width:100%;" src="images/img3.jpg" alt="picture 1">
+    </div>
+
+    <div class="carousel-item item">
+      <img src="images/img4.jpg" style="height: 400px; width:100%;" alt="picture 2">
+    </div>
+
+    <div class="carousel-item item">
+      <img src="images/img5.jpg" alt="picture 3" style="height:500px; width:100%;">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </header>
+
+    <!-- Page Content -->
+    <section class="py-5">
+      <div class="container">
+        <h1>About Us</h1>
+        <p> Nearly 40 per cent of the food produced in India is wasted every year.As per the Food Waste Index Report 2,02,150 kg of food is thrown away per person every year in Indian homes.
+            
+        </p>
+        <p>We are expanding our network ,to reach out people linking available sources of surplus food through existing  charitable organizations</p>
+        <p>Our aim is to link  food service donors with surplus food to local hunger relief agencies</p>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    
+
+  </body>
+
+</html>
+
+
+<!-- assets -->    
+    
+    <!-- <script src="assets/js/bootstrap.bundle.js"></script> -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="assets/js/bootstrap.js"></script> -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/source.bootstrap.min.js"></script>
+    <!-- <script src="assets/js/contact_us.js"></script> -->
+    <!-- <script src="assets/js/dropdown.js"></script> -->
+    <!-- <script src="assets/js/jquery.js"></script> -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- <script src="assets/js/payment.js"></script> -->
+    <!-- <script src="assets/js/restaurant.js"></script> -->
+    <!-- <script src="assets/js/result.js"></script> -->
+    <!-- <script src="assets/js/review.js"></script> -->
+    <!-- <script src="assets/js/sign_in.js"></script> -->
+    <!-- <script src="assets/js/sign_up.js"></script> -->
+
+    <!-- <link href="assets/css/account_header.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/animate.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/bootstrap.css" rel="stylesheet"> -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/source.bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="assets/css/bootstrap-grid.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/bootstrap-grid.min.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/bootstrap-reboot.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/bootstrap-reboot.min.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/checkout.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/check-radio.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/contact_us.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/delivery.css" rel="stylesheet"> -->
+    <link href="assets/css/half-slider.css" rel="stylesheet">
+    <link href="assets/css/index.css" rel="stylesheet">
+    <!-- <link href="assets/css/payment.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/profile.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/result.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/review.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/sign_in.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/sign_up.css" rel="stylesheet"> -->
